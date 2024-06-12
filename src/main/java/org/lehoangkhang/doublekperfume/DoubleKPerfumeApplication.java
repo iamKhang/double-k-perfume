d360f00917f66c4ee611b89548e7d2540e1cbb46
iamKhang <iamhoangkhang@icloud.com>
2024-06-12 15:36:31 +0700

    Setup Project

---
+++ src/main/java/org/lehoangkhang/doublekperfume/DoubleKPerfumeApplication.java
@@ -0,0 +1,14 @@
+package org.lehoangkhang.doublekperfume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoubleKPerfumeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoubleKPerfumeApplication.class, args);
+
+    }
+
+}
