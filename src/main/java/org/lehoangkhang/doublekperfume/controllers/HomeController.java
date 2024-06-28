aa1e764267274dabedbb474d923ffe80ac4e0286
iamKhang <iamhoangkhang@icloud.com>
2024-06-28 21:00:45 +0700

    Gắn trang đăng nhập

--- src/main/java/org/lehoangkhang/doublekperfume/controllers/HomeController.java
+++ src/main/java/org/lehoangkhang/doublekperfume/controllers/HomeController.java
@@ -7,6 +7,7 @@ import org.lehoangkhang.doublekperfume.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,5 +21,11 @@ public class HomeController {
         List<Product> products = productService.getAllProducts();
         model.addAttribute("products", products);
         return "user/index";
+    }
+
+    // Vào trang đăng nhập
+    @GetMapping("/login")
+    public String login() {
+        return "user/pages/loginPage";
     }
 }
